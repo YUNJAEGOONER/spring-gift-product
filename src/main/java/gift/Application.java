3fff64147a1f63975dcc8b288c2ae3d271f0228d
YunjaeLee <yunjaelee@YunjaeLeeui-MacBookAir.local>
2025-06-23 16:24:07 +0900

    add testController

--- src/main/java/gift/Application.java
+++ src/main/java/gift/Application.java
@@ -2,10 +2,16 @@ package gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @RestController
+    class testController{
+
     }
 }
